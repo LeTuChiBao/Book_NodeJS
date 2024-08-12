f93f1cb0a201a9f8abd9c616d1e27764e68e8951
LeTuChiBao <bao.le@discoveryloft.com>
2024-08-12 18:47:55 +0700

    Update Time Zone VN

--- connection_db.js
+++ connection_db.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('store', 'root', null, {
+const sequelize = new Sequelize('stores', 'root', null, {
   host: 'localhost',
   dialect: 'mysql',
-  logging : false
+  logging : false,
+  timezone: '+07:00'
 });
 
 const connectionDB = async ()=> {
